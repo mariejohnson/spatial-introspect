5e8102a63510fc331663eae4b5da2301efefe2b4
Marie Johnson <mariejohnson@nceas.ucsb.edu>
2018-09-19 16:50:53 -0700

    updated function get_shp_extents

--- pycharm/get_extent.py
+++ pycharm/get_extent.py
@@ -3,10 +3,26 @@ import rasterio
 from rasterio import RasterioIOError
 import os
 
-# practicing pushing via pycharm
 
-shp_fn = "/home/shares/scientist/sasap-biophys/HabitatMetrics/glaciers/Glaciers_RAP.shp"
+shp_fn = "/Users/datateam/repos/spatial-introspect/test_data/HerdSpatialDistribution/HerdSpatialDistribution.shp"
 
+def get_shp_extents(shapefile):
+    lats = []
+    lons = []
+    with fiona.open(shapefile, 'r') as src:
+        for feat in src:
+            coords = feat['geometry']['coordinates'][0] # "I usually stop the debugger on this line, and see what's inside `feat` so get the values I'm after, whether they are geometry or property attributes"
+            [(lons.append(x), lats.append(y)) for x, y in coords]
+
+    min_lat, max_lat = min(lats), max(lats)
+    min_lon, max_lon = min(lons), max(lons)
+    print('South extent: {}\nNorth extent: {}\nWest extent: {}\nEast extent: {}'.format(min_lat,
+                                                                                        max_lat,
+                                                                                        min_lon,
+                                                                                        max_lon))
+get_shp_extents(shp_fn)
+
+'''
 def get_extent_for_raster(file_path):
     ds = rasterio.open(file_path)
     # do stuff with ds (data source) to get extent
@@ -31,7 +47,7 @@ def get_extent_for_csv(file_path, long_col_name=None, lat_col_name=None):
     if long_col_name is None:
         # do stuff to figure out the long_col_name
         pass
-
+'''
 def get_extent_if_possible(file_path):
     try:
         extent = get_extent_for_vector(file_path)
