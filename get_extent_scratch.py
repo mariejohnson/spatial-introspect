70335f1e650d5c429711382388a2f140d11ed184
Marie Johnson <mariejohnson@nceas.ucsb.edu>
2018-09-26 16:06:44 -0700

    pandas/geopandas scratch and notes

--- get_extent_scratch.py
+++ get_extent_scratch.py
@@ -1,9 +1,24 @@
 import fiona
+from fiona import errors
 import rasterio
 from rasterio import RasterioIOError
 import os
+import pandas as pd
+import numpy as np
+import geopandas
+from geopandas import GeoDataFrame
+import shapely
+from shapely.geometry import Point
+import sys
+import matplotlib
+import matplotlib.cbook
 
 
+shp_fn = "/Users/datateam/repos/spatial-introspect/test_data/HerdSpatialDistribution/HerdSpatialDistribution.shp"
+rast_ex = "/Users/datateam/repos/spatial-introspect/test_data/NE1_50M_SR/NE1_50M_SR.tif"
+CA_rast = "/Users/datateam/repos/spatial-introspect/NE1_50M_SR_W_tenth_CA.tif"
+csv_ex = "/Users/datateam/repos/spatial-introspect/test_data/sample_d1_files/urn_uuid_5bb3f86b_ef85_447f_a026_6d8eb6306ea4/data/huayhuash.5.5-2010-11_Huayhuash_SitiosMuestreoPasto.csv.csv"
+NM_rast = "/Users/datateam/Desktop/test_data/soil_color_NM/NM_125cm.tif"
 # my scratch paper or things I didn't want to just delete (some of it might be useful at some point)
 
 shp_fn = "/Users/datateam/repos/spatial-introspect/test_data/HerdSpatialDistribution/HerdSpatialDistribution.shp"
@@ -97,3 +112,123 @@ def get_extent_csv(file_path):
         print(coords)
 
 get_extent_csv(csv_ex)
+
+#Transform coordinates
+from pyproj import Proj, transform
+
+inProj = Proj(init='epsg:3857')
+outProj = Proj(init='epsg:4326')
+x1,y1 = -11705274.6374,4826473.6922
+x2,y2 = transform(inProj,outProj,x1,y1)
+print x2,y2
+
+
+# Now I need to turn this into a function and make it into a bounding box
+df = pd.read_csv("/Users/datateam/repos/spatial-introspect/test_data/sample_d1_files/urn_uuid_5bb3f86b_ef85_447f_a026_6d8eb6306ea4/data/huayhuash.5.5-2010-11_Huayhuash_SitiosMuestreoPasto.csv.csv")
+geometry = [Point(xy) for xy in zip(df.Lat, df.Long)]
+df = df.drop(['Lat', 'Long'], axis=1)
+crs = {'init': 'epsg:4326'}
+gdf = GeoDataFrame(df, crs=crs, geometry=geometry)
+print(gdf)
+
+
+#Start over with directions from website: http://geopandas.org/gallery/create_geopandas_from_pandas.html#sphx-glr-gallery-create-geopandas-from-pandas-py
+
+df2 = pd.read_csv("/Users/datateam/repos/spatial-introspect/test_data/sample_d1_files/urn_uuid_5bb3f86b_ef85_447f_a026_6d8eb6306ea4/data/huayhuash.5.5-2010-11_Huayhuash_SitiosMuestreoPasto.csv.csv")
+
+df2['coordinates'] = list(zip(df2.Long, df2.Lat))
+
+df2['coordinates'] = df2['coordinates'].apply(Point)
+
+gdf2 = geopandas.GeoDataFrame(df2, geometry='coordinates')
+
+
+bounds1 = gdf2.bounds
+#bounds1 is a dataframe
+print(bounds1)
+# Not exactly what I want but maybe. I think I want (out of the entire list) (minx, miny) and (maxx, maxy)
+# actually that'd be fairly easy to convert here I think.
+
+#Just goofing around with pandas.
+minx = bounds1['minx']
+print(minx)
+
+min_minx = minx.min
+print(min_minx)
+
+bounds1.min['minx']
+
+#This: http://geopandas.org/gallery/create_geopandas_from_pandas.html#sphx-glr-gallery-create-geopandas-from-pandas-py
+
+#To find the name of the geometry which is the geoseries
+#gdf.geometry.name
+
+#This worked but I'm not sure that it is actually a geoseries
+gs = gdf.geometry
+print(gs)
+
+
+bnds = geopandas.gs.bounds
+
+gs1 = geopandas.GeoSeries(gdf)
+
+
+# this is unsuccessful
+def get_extent_csv(file_path):
+    df = pd.read_csv(file_path)
+    for col in df: #hmm I think I need it to skip the first row, possibly by setting index, I think this is already default?
+        coords = df.iterrows()
+        print(coords)
+
+get_extent_csv(csv_ex)
+
+def get_bounds_csv(filepath):
+    df = pd.read_csv(filepath)
+    df['coordinates'] = list(zip(df.Long, df.Lat))
+    df['coordinates'] = df['coordinates'].apply(Point)
+    gdf = geopandas.GeoDataFrame(df, geometry='coordinates')
+    bnds = gdf.bounds
+    print(bnds)
+
+
+df = pd.read_csv("/Users/datateam/repos/spatial-introspect/test_data/sample_d1_files/urn_uuid_5bb3f86b_ef85_447f_a026_6d8eb6306ea4/data/huayhuash.5.5-2010-11_Huayhuash_SitiosMuestreoPasto.csv.csv")
+
+df['coordinates'] = list(zip(df.Long, df.Lat))
+
+df['coordinates'] = df['coordinates'].apply(Point)
+
+gdf = geopandas.GeoDataFrame(df, geometry='coordinates')
+
+
+bnds = gdf.bounds
+#bnds is a dataframe
+print(bnds)
+# Not exactly what I want but maybe. I think I want (out of the entire list) (minx, miny) and (maxx, maxy)
+# actually that'd be fairly easy to convert here I think.
+
+total_bnds = gdf.total_bounds
+print(total_bnds)
+
+'''
+This: https://gis.stackexchange.com/questions/174159/convert-a-pandas-dataframe-to-a-geodataframe
+from geopandas import GeoDataFrame
+from shapely.geometry import Point
+
+geometry = [Point(xy) for xy in zip(df.Lon, df.Lat)]
+df = df.drop(['Lon', 'Lat'], axis=1)
+crs = {'init': 'epsg:4326'}
+gdf = GeoDataFrame(df, crs=crs, geometry=geometry)
+
+Maybe this: https://gis.stackexchange.com/questions/114066/handling-kml-csv-with-geopandas-drivererror-unsupported-driver-ucsv
+
+import pandas as pd
+import geopandas as gp
+from shapely.geometry import Point
+
+stations = pd.read_csv('../data/stations.csv')
+stations['geometry'] = stations.apply(lambda z: Point(z.X, z.Y), axis=1)
+stations = gp.GeoDataFrame(stations)
+
+Maybe this: https://gist.github.com/tchajed/93c347aeb2b24f034ea3
+
+'''
