5291e1a2214c5b90881014ff66743501823a6d6a
Marie Johnson <mariejohnson@nceas.ucsb.edu>
2018-10-05 15:31:41 -0700

    removed previous get_extent_for_vector. Constructing new get_extent_vector fuction that unprojects coordinates if needed. Started using bounds package

--- get_extent.py
+++ get_extent.py
@@ -14,90 +14,70 @@ import sys
 import matplotlib
 import matplotlib.cbook
 import re
+from fiona._transform import _transform, _transform_geom
+from pyproj import Proj
 from bounds import RasterBounds
-from fiona.transform import transform
+from bounds import GeoBounds
 import bounds
 
 
 
-shp_fn = "/Users/datateam/repos/spatial-introspect/test_data/HerdSpatialDistribution/HerdSpatialDistribution.shp"
-rast_ex = "/Users/datateam/repos/spatial-introspect/test_data/NE1_50M_SR/NE1_50M_SR.tif"
-CA_rast = "/Users/datateam/repos/spatial-introspect/NE1_50M_SR_W_tenth_CA.tif"
-csv_ex = "/Users/datateam/repos/spatial-introspect/test_data/sample_d1_files/urn_uuid_5bb3f86b_ef85_447f_a026_6d8eb6306ea4/data/huayhuash.5.5-2010-11_Huayhuash_SitiosMuestreoPasto.csv.csv"
+shp_fn = "/Users/datateam/repos/spatial_introspect/test_data/HerdSpatialDistribution/HerdSpatialDistribution.shp"
+rast_ex = "/Users/datateam/repos/spatial_introspect/test_data/NE1_50M_SR/NE1_50M_SR.tif"
+CA_rast = "/Users/datateam/repos/spatial_introspect/NE1_50M_SR_W_tenth_CA.tif"
+csv_ex = "/Users/datateam/repos/spatial_introspect/test_data/sample_d1_files/urn_uuid_5bb3f86b_ef85_447f_a026_6d8eb6306ea4/data/huayhuash.5.5-2010-11_Huayhuash_SitiosMuestreoPasto.csv.csv"
 NM_rast = "/Users/datateam/Desktop/test_data/soil_color_NM/NM_125cm.tif"
 sampling_sites = "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/huayhuash.5.5"
 csv_no_geo = "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/huayhuash.10.2"
-
 shp_NM = "/Users/datateam/Desktop/snotel_201701_1169_shp/snotel_201701_1169.shp"
 
-bounds1 = RasterBounds(raster=rast_ex)
+
+# need to turn this into a function
+# One problem will be if it is not a polygon (or maybe that is dealt with?) or maybe that doesn't matter
+def get_extent_vector(shapefile):
+    with fiona.open(shapefile, 'r') as src:
+        bb = src.bounds
+        bbox = list(bb)
+        west = []
+        south = []
+        east = []
+        north = []
+        for i in bbox: #this doesn't exactly work
+            west.append(bbox[0])
+            south.append(bbox[1])
+            east.append(bbox[2])
+            north.append(bbox[3])
+
+            print(bbox[i]) # Need to turn this into w, s, e, n
+            # maybe print i? No. I'm thinking about this wrong
+
+get_extent_vector(shp_fn)
+
+
+
+
+shp_file = fiona.open(shp_fn, 'r')
+fiona_bb = shp_file.bounds
+print(fiona_bb) # Need to turn this into w, s, e, n
+
+with fiona.open(shapefile, 'r') as src:
+    for bbox in src:
+        fiona_bb = shp_file.bounds
+
+NM_shp_file = fiona.open(shp_NM, 'r')
+fiona_bb = shp_file.bounds
+print(fiona_bb) # Need to turn this into w, s, e, n
+
+bbox = bounds.GeoBounds(west=9085047, south=11971301, east=10104624, north=12228183)
+geo_bounds = bbox.to_geographic(epsg=3857)
+print(geo_bounds)
+
+
+# Need to convert when using files like NM_raster that are in meters, this does that
+
+bounds1 = RasterBounds(raster=CA_rast)
 print(bounds1)
 
-
-# for both vector and raster I need to reproject
-
-class MyException(Exception):
-    pass
-
-
-def get_extent_for_vector(shapefile):
-    lats = []
-    lons = []
-    with fiona.open(shapefile, 'r') as src:
-        for feat in src:
-            # if ['geometry'] is not type Point:
-            #     raise MyException('Point data') look at formatting here: https://github.com/Toblerity/Fiona/blob/master/fiona/transform.py
-            # work on this later, work on transforming first
-            coords = feat['geometry']['coordinates'][0] # stop the debugger on this line, and see what's inside `feat` to see the values I'm after
-            [(lons.append(x), lats.append(y)) for x, y in coords]
-            # transform('EPSG:4326', 'EPSG:4326', xs=lons, ys=lats) # this might need to be x, y
-            print(lats, lons)
-
-    # min_lat, max_lat = min(lats), max(lats)
-    # min_lon, max_lon = min(lons), max(lons)
-    # print('South extent: {}\nNorth extent: {}\nWest extent: {}\nEast extent: {}'.format(min_lat,
-    #                                                                                     max_lat,
-    #                                                                                     min_lon,
-    #                                                                                     max_lon))
-get_extent_for_vector(shp_fn)
-
-
-# Practice using transform before putting it in a function
-abc = fiona.open(shp_fn, 'r')
-rec = next(abc)
-print(rec['geometry']['coordinates'])
-lats = []
-lons = []
-crds = rec['geometry']['coordinates']
-crds0 = rec['geometry']['coordinates'][0] # [0] gets rid of one of the brackets
-[(lons.append(x), lats.append(y)) for x, y in coords] # I think I need to iterate through the list to extract x, y
-print(crds0)
-
-for
-
-coords = ['coordinates'][0] # stop the debugger on this line, and see what's inside `feat` to see the values I'm after
-[(lons.append(x), lats.append(y)) for x, y in coords]
-transform('EPSG:4326', 'EPSG:26953', lons, lats)
-
-
-from shapely.geometry import shape
-c = fiona.open("/Users/datateam/repos/spatial-introspect/test_data/HerdSpatialDistribution/HerdSpatialDistribution.shp")
-# first record
-country = c.next()
-print(country)
-print(c)
-c.bounds
-c.next
-
-shapely.geometry.shape(c)
-
-
-
-# Need to convert when using files like NM_raster that are in meters
-'''rightm but that's probably what you're going to want. if you want to find it's bounds in geographic coordinates you need to 
-'reproject' the coordinates you have, or reproject the entire raster. 
-rasterio will reproject the raster, if you want to do that programmatically. 
-or you can use `pyproj` to convert the coordinates directly'''
 def get_extent_for_raster(raster):
     with rasterio.open(raster, 'r') as ds:
         #ds.features.bounds(geometry = polygon, north_up = True, transform = None)
@@ -105,6 +85,8 @@ def get_extent_for_raster(raster):
         print(ds.meta) #gets more info about the raster
 
 get_extent_for_raster(NM_rast)
+
+
 
 
 def get_extent(file_path):
