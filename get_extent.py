40cc2e2db81fd5000985c009dc1777a7be9875cd
Marie Johnson <mariejohnson@nceas.ucsb.edu>
2018-10-14 20:39:23 -0600

    it works! get_extent for vector, raster and CSV work! output is a list of geographic bounds.

--- get_extent.py
+++ get_extent.py
@@ -1,65 +1,50 @@
-import fiona
-from fiona import errors
-import rasterio
-from rasterio import RasterioIOError
-from rasterio import features
-import os
-import pandas as pd
-import numpy as np
-import geopandas as gpd
-from geopandas import GeoDataFrame
-import shapely
-from shapely.geometry import Point
-import sys
 import re
-from fiona._transform import _transform, _transform_geom
-from pyproj import Proj
-from vector_bounds import RasterBounds, GeoBounds, VectorBounds
-import bounds
+
+import fiona
+import geopandas as gpd
+import pandas as pd
+from fiona import errors
+from pandas import errors
+from shapely.geometry import Point
+
+from vector_bounds import RasterBounds, VectorBounds
+
+shp_fn = "/Users/datateam/repos/spatial_introspect/test_data/HerdSpatialDistribution/HerdSpatialDistribution.shp"
+rast_ex = "/Users/datateam/repos/spatial_introspect/test_data/NE1_50M_SR/NE1_50M_SR.tif"
+CA_rast = "/Users/datateam/repos/spatial_introspect/NE1_50M_SR_W_tenth_CA.tif"
+csv_ex = "/Users/datateam/repos/spatial_introspect/test_data/SitiosMuestreoPasto.csv"
+NM_rast = "/Users/datateam/Desktop/test_data/soil_color_NM/NM_125cm.tif"
+sampling_sites = "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/huayhuash.5.5"
+csv_no_geo = "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/huayhuash.10.2"
+shp_NM = "/Users/datateam/Desktop/snotel_201701_1169_shp/snotel_201701_1169.shp"
 
 
 # Issue: if it's not a polygon
 def get_extent_vector(shapefile):
     vb = VectorBounds(shapefile)
     bounds = list((vb.west, vb.south, vb.east, vb.north))
-    #print(bounds)
     return bounds
 
-get_extent_vector(shp_fn)
+
+# get_extent_vector(shp_NM)
 
 
 def get_extent_raster(raster):
     rbnds = RasterBounds(raster)
     bounds = list((rbnds.west, rbnds.south, rbnds.east, rbnds.north))
-    #print(bounds)
+    # print(bounds)
     return bounds
 
-get_extent_raster(CA_rast)
 
+# get_extent_raster(CA_rast)
 
-# **************************************************************************************************** #
-
-
-def get_extent(file_path):
-    try:
-        extent = get_extent_vector(file_path)
-    except errors.FionaValueError: # this will raise an exception if it's a CSV "unsupported driver"
-        extent = get_extent_raster(file_path)
-    return extent
-
-
-
-get_extent(shp_fn)
-
-get_extent(CA_rast)
-
-get_extent(csv_ex)
 
 # # # CSV # # #
 
 # Extracts extent if lat and lon exist in column names
 def get_extent_csv(csv):
     csv_df = pd.read_csv(csv)
+
     class MyException(Exception):
         pass
 
@@ -88,4 +73,34 @@ def get_extent_csv(csv):
     csv_df['geometry'] = csv_df.apply(lambda r: Point(r[lon_name], r[lat_name]), axis=1)
     pnt_gdf = gpd.GeoDataFrame(csv_df)
 
-    return pnt_gdf.total_bounds
+    return pnt_gdf.total_bounds.tolist()
+
+
+# **************************************************************************************************** #
+
+# works but does not include extent for CSV
+# def get_extent(file_path):
+#     try:
+#         extent = get_extent_vector(file_path)
+#     except errors.FionaValueError: # this will raise an exception if it's a CSV "unsupported driver"
+#         extent = get_extent_raster(file_path)
+#     return extent
+
+
+def get_extent(file_path):
+    try:
+        extent = get_extent_csv(file_path)
+    except pd.errors.ParserError:
+        try:
+            extent = get_extent_vector(file_path)
+        except fiona.errors.FionaValueError:
+            extent = get_extent_raster(file_path)
+    return extent
+
+
+# get_extent(shp_fn)
+
+print(get_extent(csv_ex))
+
+# print(get_extent(CA_rast))  # does not work for raster
+
